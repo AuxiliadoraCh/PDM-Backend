6146d947949915919f8ebfffa2637d1064f8838c
GuillermoCaceres3 <00132623@uca.edu.sv>
2025-06-23 03:11:58 -0600

    de regreso a pruebas locales

--- src/app.js
+++ src/app.js
@@ -21,6 +21,6 @@ app.use("/api/payment",paymentRoutes)
 app.use('/api/promotions',promotionRoutes)
 app.use('/api/used_coupons', userCouponRoutes) 
 
-app.listen(3000, '0.0.0.0', () => {
+app.listen(3000, () => {
   console.log('Server running on port 3000')
 })
