fb464c38b5878d0089a4df3541e115c1d06383d3
GuillermoCaceres3 <00132623@uca.edu.sv>
2025-06-23 02:40:50 -0600

    pruebas para deploy

--- src/app.js
+++ src/app.js
@@ -21,6 +21,6 @@ app.use("/api/payment",paymentRoutes)
 app.use('/api/promotions',promotionRoutes)
 app.use('/api/used_coupons', userCouponRoutes) 
 
-app.listen(3000, () => {
+app.listen(3000, '0.0.0.0', () => {
   console.log('Server running on port 3000')
 })
